f4a46210efad2cfb54459e4166434722d0197cd7
Gq9527 <1046079096@qq.com>
2017-10-04 15:54:46 +0800

    xiangyingshi

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by q on 2017/10/2.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
